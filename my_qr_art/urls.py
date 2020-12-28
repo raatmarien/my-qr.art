747ae76e4655f2463d8ae36b91e31c2d228ff8d4
Marien Raat <mail@marienraat.nl>
2020-12-28 22:35:42 +0100

    Make redirect url case insensitive

--- my_qr_art/urls.py
+++ my_qr_art/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('R/', include('redirect.urls')),
+    path('r/', include('redirect.urls')),
     path('', include('qr_app.urls')),
 ]
