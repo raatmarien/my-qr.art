bbe5503bdf4b52fc4f846ab052bcac53ff071116
Marien Raat <mail@marienraat.nl>
2020-12-28 22:33:56 +0100

    Add capital redirect

--- my_qr_art/urls.py
+++ my_qr_art/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('r/', include('redirect.urls')),
+    path('R/', include('redirect.urls')),
     path('', include('qr_app.urls')),
 ]
